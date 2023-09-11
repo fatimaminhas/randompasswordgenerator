c2af36e80b2a475910f4a30207aeb810d5952007
fatimaminhas <f.minhas394@gmail.com>
2023-09-11 17:54:39 -0400

    added updates to js script and css

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// Assignment code here
-var index = Math.floor(Math.random() * special.length);
-console.log(index);
+//Assignment code here
+
 var lower = "abcdefghijklmnopqrstuvwxyz";
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-//console.log(special[index]);
-// between 8 and 128 characters
+var randomPassword = "";
+
 var characterBank = "";
 //PROMPTS
 //lowercase
@@ -16,22 +15,37 @@ var characterBank = "";
 function generatePassword() {
   console.log("Hello from generatePassword");
 
-  var passwordLength = window.prompt(
-    "How many characters do you want the password to be? (pick a number between 8 and 128)"
+  var keyLength = prompt(
+    "Your password should be between 8 to 128 characters long. Please enter your desired number of characters below."
   );
-  console.log("password length", passwordLength);
+  console.log("password length", keyLength);
 
-  var confirmLower = window.confirm("....... lowercase");
-  console.log(confirmLower);
+  if (keyLength < 8 || keyLength > 128) {
+    alert(
+      "Your password must be between 8 and 128 characters, please enter another number."
+    );
+    return;
+  } else {
+    var confirmLower = window.confirm(
+      "Do you want lowercase letters in your password?"
+    );
+    console.log(confirmLower);
 
-  var confirmUpper = window.confirm("....... uppercase");
-  console.log(confirmUpper);
+    var confirmUpper = window.confirm(
+      "Do you want uppercase letters in your password?"
+    );
+    console.log(confirmUpper);
 
-  var confirmNumerical = window.confirm("....... numerical");
-  console.log(confirmNumerical);
+    var confirmNumerical = window.confirm(
+      "Do you want numbers in your password?"
+    );
+    console.log(confirmNumerical);
 
-  var confirmSpecial = window.confirm("....... special");
-  console.log(confirmSpecial);
+    var confirmSpecial = window.confirm(
+      "Do you want special characters in your password?"
+    );
+    console.log(confirmSpecial);
+  }
 
   if (confirmLower === true) {
     console.log("Before", characterBank);
@@ -57,16 +71,25 @@ function generatePassword() {
     console.log("After", characterBank);
   }
   //return "Look it's a password!";
+
+  for (i = 0; i < keyLength; i++) {
+    randomPassword +=
+      characterBank[Math.floor(Math.random() * keyLength.length)];
+  }
 }
+// var index = Math.floor(Math.random() * special.length);
+// console.log(index);
+//console.log(special[index]);
+
 // Get references to the #generate element
 var generateBtn = document.getElementById("generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(randomPassword) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = randomPassword;
 }
 
 // Add event listener to generate button
